f5de9ad3a923aaab8bf9caba4cb2785dbd842212
wind-ing <53858130+wind-ing@users.noreply.github.com>
2022-06-25 18:00:57 +0800

    排序部分增写

---
+++ ACM/Code/code 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int n, s(0);
+    cin >> n;
+    int a[100];
+    for(int i=0; i<n; i++)
+        cin >> a[i];
+    sort(a, a+n);
+    for(int j=0; j<n; j=j+2)
+        s += a[j+1] - a[j];
+    cout << s;
+    return 0;
+}
